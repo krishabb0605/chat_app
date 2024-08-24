5feed929c80f87534bad9b6b4ad8757c4f4cfd2f
krisha bhingaradiya <krishabhingaradiya1234@gmail.com>
2024-08-24 12:05:16 +0530

    Register related changes

--- client/src/layout/index.js
+++ client/src/layout/index.js
@@ -1,9 +1,12 @@
 import React from 'react';
+import logo from '../assets/logo.png';
 
 const AuthLayouts = ({ children }) => {
   return (
     <>
-      <div>logo</div>
+      <header className='flex justify-center items-center py-3 h-20 shadow-md bg-white'>
+        <img src={logo} alt='Logo' width={180} height={60} />
+      </header>
       {children}
     </>
   );
--- client/src/pages/RegisterPage.js
+++ client/src/pages/RegisterPage.js
@@ -1,11 +1,155 @@
-import React from 'react'
+import React, { useState } from 'react';
+import { IoClose } from 'react-icons/io5';
+import { Link } from 'react-router-dom';
+import uploadFile from '../helpers/uploadFile';
+import axios from 'axios';
+import toast from 'react-hot-toast';
 
 const RegisterPage = () => {
-  return (
-    <div>
-      Register Page
-    </div>
-  )
-}
+  const [data, setData] = useState({
+    name: '',
+    email: '',
+    password: '',
+    profile_pic: '',
+  });
+  const [uploadPhoto, setUploadPhoto] = useState('');
 
-export default RegisterPage
+  const handleOnChange = (event) => {
+    const { name, value } = event.target;
+    setData((prev) => {
+      return {
+        ...prev,
+        [name]: value,
+      };
+    });
+  };
+
+  const handleUploadPhoto = async (event) => {
+    const file = event.target.files[0];
+    const uploadPhoto = await uploadFile(file);
+
+    setData((prev) => {
+      return {
+        ...prev,
+        profile_pic: uploadPhoto?.url,
+      };
+    });
+
+    setUploadPhoto(file);
+  };
+
+  const handleClearUploadPhoto = (event) => {
+    setUploadPhoto('');
+    event.stopPropagation();
+    event.preventDefault();
+  };
+
+  const handleSubmit = async (event) => {
+    event.preventDefault();
+    event.stopPropagation();
+
+    const URL = `${process.env.REACT_APP_BACKEND_URL}/api/register`;
+    try {
+      const response = await axios.post(URL, data);
+      toast.success(response.data.message);
+      console.log('response', response);
+    } catch (error) {
+      toast.error(error?.response?.data?.message);
+      console.log({ error });
+    }
+    console.log({ data });
+  };
+
+  return (
+    <div className='mt-5'>
+      <div className='bg-white w-full  max-w-md rounded overflow-hidden p-4 mx-auto '>
+        <h3>Welcome to Chat App !</h3>
+        <form className='grid gap-4 mt-5' onSubmit={handleSubmit}>
+          <div className='flex flex-col gap-1'>
+            <label htmlFor='name'>Name :</label>
+            <input
+              type='text'
+              id='name'
+              name='name'
+              placeholder='Enter your name'
+              className='bg-slate-100 px-2 py-1 focus:outline-primary'
+              value={data.name}
+              onChange={handleOnChange}
+              required
+            />
+          </div>
+
+          <div className='flex flex-col gap-1'>
+            <label htmlFor='name'>Email :</label>
+            <input
+              type='email'
+              id='email'
+              name='email'
+              placeholder='Enter your email'
+              className='bg-slate-100 px-2 py-1 focus:outline-primary'
+              value={data.email}
+              onChange={handleOnChange}
+              required
+            />
+          </div>
+
+          <div className='flex flex-col gap-1'>
+            <label htmlFor='email'>Password :</label>
+            <input
+              type='password'
+              id='password'
+              name='password'
+              placeholder='Enter your password'
+              className='bg-slate-100 px-2 py-1 focus:outline-primary'
+              value={data.password}
+              onChange={handleOnChange}
+              required
+            />
+          </div>
+
+          <div className='flex flex-col gap-1'>
+            <label htmlFor='profile_pic'>
+              Photo :
+              <div className='h-14 bg-slate-200 flex justify-center items-center border rounded hover:border-primary cursor-pointer'>
+                <p className='text-sm max-w-[300px] text-ellipsis line-clamp-1'>
+                  {uploadPhoto?.name
+                    ? uploadPhoto.name
+                    : 'Upload profile photo'}
+                </p>
+
+                {uploadPhoto?.name && (
+                  <button
+                    className='text-lg ml-2 hover:text-red-600'
+                    onClick={handleClearUploadPhoto}
+                  >
+                    <IoClose />
+                  </button>
+                )}
+              </div>
+            </label>
+
+            <input
+              type='file'
+              id='profile_pic'
+              name='profile_pic'
+              className='bg-slate-100 px-2 py-1 focus:outline-primary hidden'
+              onChange={handleUploadPhoto}
+            />
+          </div>
+
+          <button className='bg-primary text-lg  px-4 py-1 rounded mt-2 font-bold leading-relaxed tracking-wide text-white hover:bg-secondary'>
+            Register
+          </button>
+        </form>
+        <p className='my-3 text-center'>
+          Already have account ?{' '}
+          <Link to='/email' className='hover:text-primary font-semibold '>
+            Login
+          </Link>
+        </p>
+      </div>
+    </div>
+  );
+};
+
+export default RegisterPage;
